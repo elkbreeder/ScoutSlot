23d61f2abf09662f5199d8a2c8b04e1d0e0a3534
Gregor Ziegltrum <gregi11@web.de>
2019-03-17 16:08:32 +0100

    two quite working rolls

--- PythonSrc/main.py
+++ PythonSrc/main.py
@@ -1,20 +1,34 @@
 import sys
 import pygame
+import random
 
 from PythonSrc.reel import Reel
+cardSize = (200, 300)
+roll_offset = 0
+roll_speed_range = (5, 10)
 
 pygame.init()
-cardSize = (200, 300)
-frameSize = (cardSize[1] * 2, 500)
+
+frameSize = (cardSize[0] * 2, cardSize[1] * 3)
 screen = pygame.display.set_mode(frameSize)
 
-reel = Reel(screen)
+reel = [Reel(screen, cardSize, roll_offset), Reel(screen, cardSize, roll_offset + cardSize[0])]
+roll = [0, 0]
+roll_speed = [0, 0]
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                if all(i == 0 for i in roll):
+                    roll[:] = map((lambda _: random.randint(100, 200)), roll)
+                    roll_speed[:] = map((lambda _: random.randint(roll_speed_range[0], roll_speed_range[1])), roll_speed)
     screen.fill((0, 0, 0))  # fill black
-    reel.draw()
-    reel.move(3)
+    for i in range(0, len(reel)):
+        if roll[i] > 0:
+            roll[i] = roll[i] - 1
+            reel[i].move(roll_speed[i])
+        reel[i].draw()
     pygame.display.flip()
--- PythonSrc/reel.py
+++ PythonSrc/reel.py
@@ -2,31 +2,41 @@ import pygame;
 
 
 class Reel:
-    def __init__(self, screen):
-        self.reelPosition = 0
+    def __init__(self, screen, card_size, x):
+        self.card_size = card_size
+        self.reel_position = 0
         self.screen = screen
         reel = []
         for i in range(0, 4):
             curr = pygame.image.load("../cards200x300/pic" + str(i) + ".png")
-            currRect = curr.get_rect()
-            currRect = currRect.move((0, -currRect.height * i))
-            reel.append([curr, currRect])
-
+            curr_rect = curr.get_rect()
+            if curr_rect.size != card_size:
+                raise Exception("wrong image format", "../cards200x300/pic" + str(i) + ".png has the wrong format")
+            curr_rect = curr_rect.move((x, -curr_rect.height * i))
+            card_id = str(i)
+            reel.append([curr, curr_rect, card_id])
         self.reel = reel
+        self.move(card_size[1])
+
+    def get_current_id(self):
+        return self.reel[self.reel_position][2]
 
     def draw(self):
         for curr in self.reel:
             self.screen.blit(curr[0], curr[1])
 
     def move(self, y):
+        if y > self.reel[0][1].height:
+            for _ in range(1, y//self.card_size[1]):
+                self.move(self.card_size[1])
         for curr in self.reel:
             curr[1].move_ip((0, y))
-        reelPositionRect = (self.reel[self.reelPosition])[1]
-        if reelPositionRect.bottomleft[1] > reelPositionRect.height * 2:  # if currentreelposition leaves screen
+        reel_position_rect = (self.reel[self.reel_position])[1]
+        if reel_position_rect.bottomleft[1] > reel_position_rect.height * 2:  # if self.reel_position leaves screen
 
-            self.reelPosition = (self.reelPosition + 1) % len(self.reel)  # update reel Position
-            reelPositionRect = (self.reel[self.reelPosition])[1]
-            newPosition = reelPositionRect.topleft[1] - reelPositionRect.height  # determine where the 2nd reel after
-            # the current reel position at the beginning has to be placed to make the reel goes round and round
-            movePositionRect = (self.reel[(self.reelPosition + 1) % len(self.reel)])[1]
-            movePositionRect.y = newPosition  # place the 2nd reel
+            self.reel_position = (self.reel_position + 1) % len(self.reel)  # update reel Position
+            reel_position_rect = (self.reel[self.reel_position])[1]
+            new_position = reel_position_rect.topleft[1] - reel_position_rect.height  # determine where the 2nd reel
+            # after the current reel position at the beginning has to be placed to make the reel goes round and round
+            move_position_rect = (self.reel[(self.reel_position + 1) % len(self.reel)])[1]
+            move_position_rect.y = new_position  # place the 2nd reel
