012a1c38bf9d1c53944818206003145574b976f3
Gregor Ziegltrum <gregi11@web.de>
2019-03-17 17:00:50 +0100

    added winner detection

--- PythonSrc/main.py
+++ PythonSrc/main.py
@@ -3,19 +3,23 @@ import pygame
 import random
 
 from PythonSrc.reel import Reel
+
 cardSize = (200, 300)
 roll_offset = 0
 roll_speed_range = (5, 10)
 
 pygame.init()
 
-frameSize = (cardSize[0] * 2, cardSize[1] * 3)
+frameSize = (cardSize[0] * 3, cardSize[1] * 2)
 screen = pygame.display.set_mode(frameSize)
 
-reel = [Reel(screen, cardSize, roll_offset), Reel(screen, cardSize, roll_offset + cardSize[0])]
-roll = [0, 0]
-roll_speed = [0, 0]
 
+reel = [Reel(screen, cardSize, roll_offset),
+        Reel(screen, cardSize, roll_offset + cardSize[0]),
+        Reel(screen, cardSize, roll_offset + cardSize[0] * 2)]
+roll = [0, 0, 0]
+roll_speed = [0, 0, 0]
+result = [-1, -1, -1]
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,12 +27,26 @@ while 1:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
                 if all(i == 0 for i in roll):
+                    result[:] = map(lambda _: -1, result)
+                    roll_speed[:] = map(
+                        (lambda _: random.randint(roll_speed_range[0], roll_speed_range[1])), roll_speed)
                     roll[:] = map((lambda _: random.randint(100, 200)), roll)
-                    roll_speed[:] = map((lambda _: random.randint(roll_speed_range[0], roll_speed_range[1])), roll_speed)
     screen.fill((0, 0, 0))  # fill black
     for i in range(0, len(reel)):
         if roll[i] > 0:
             roll[i] = roll[i] - 1
             reel[i].move(roll_speed[i])
+            if roll[i] == 0 and (
+                    frameSize[1] // 5 > reel[i].get_current_rect().topleft[1] or
+                    frameSize[1] * 2 // 5 < reel[i].get_current_rect().topleft[1]):  # if the result doesn't look
+                # clear turn the wheel a bit more
+                roll[i] = 10
+            elif roll[i] == 0:
+                result[i] = reel[i].get_current_id()
+                print(str(i) + ": " + reel[i].get_current_id())
         reel[i].draw()
+    if all((i == result[0] and i is not -1) for i in result):
+        print("Winner")
+
+    pygame.draw.line(screen, (255, 255, 255), (0, frameSize[1] // 2), (frameSize[0], frameSize[1] // 2), 5)
     pygame.display.flip()
--- PythonSrc/reel.py
+++ PythonSrc/reel.py
@@ -1,4 +1,4 @@
-import pygame;
+import pygame
 
 
 class Reel:
@@ -7,36 +7,42 @@ class Reel:
         self.reel_position = 0
         self.screen = screen
         reel = []
-        for i in range(0, 4):
+        for i in range(0, 3):
             curr = pygame.image.load("../cards200x300/pic" + str(i) + ".png")
             curr_rect = curr.get_rect()
             if curr_rect.size != card_size:
                 raise Exception("wrong image format", "../cards200x300/pic" + str(i) + ".png has the wrong format")
             curr_rect = curr_rect.move((x, -curr_rect.height * i))
-            card_id = str(i)
+            card_id = str(i+1)
             reel.append([curr, curr_rect, card_id])
         self.reel = reel
-        self.move(card_size[1])
+        self.move(card_size[1] * 5)
+        print(self.reel[self.reel_position][2])
 
     def get_current_id(self):
         return self.reel[self.reel_position][2]
+
+    def get_current_rect(self):
+        return self.reel[self.reel_position][1]
 
     def draw(self):
         for curr in self.reel:
             self.screen.blit(curr[0], curr[1])
 
     def move(self, y):
-        if y > self.reel[0][1].height:
-            for _ in range(1, y//self.card_size[1]):
+        if y > self.reel[0][1].height:  # if a move is longer than 1 card_size split it up to several moves
+            for _ in range(1, y // self.card_size[1]):
                 self.move(self.card_size[1])
+            self.move(y % self.card_size[1])
+            return
         for curr in self.reel:
             curr[1].move_ip((0, y))
         reel_position_rect = (self.reel[self.reel_position])[1]
-        if reel_position_rect.bottomleft[1] > reel_position_rect.height * 2:  # if self.reel_position leaves screen
+        if reel_position_rect.bottomleft[1] > self.card_size[1] * 2:  # if self.reel_position leaves screen
 
             self.reel_position = (self.reel_position + 1) % len(self.reel)  # update reel Position
             reel_position_rect = (self.reel[self.reel_position])[1]
-            new_position = reel_position_rect.topleft[1] - reel_position_rect.height  # determine where the 2nd reel
+            new_position = reel_position_rect.topleft[1] - self.card_size[1]  # determine where the 2nd reel
             # after the current reel position at the beginning has to be placed to make the reel goes round and round
             move_position_rect = (self.reel[(self.reel_position + 1) % len(self.reel)])[1]
             move_position_rect.y = new_position  # place the 2nd reel
