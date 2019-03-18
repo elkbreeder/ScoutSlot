edce000cf45e95c5d65fa97ed08a621d617637d0
Gregor Ziegltrum <gregi11@web.de>
2019-03-18 02:42:17 +0100

    added coins

--- PythonSrc/game.py
+++ PythonSrc/game.py
@@ -5,7 +5,6 @@ import random
 from PythonSrc import gui
 from PythonSrc import model
 
-
 roll_speed_range = (5, 10)
 roll_range = (100, 200)
 
@@ -14,15 +13,17 @@ class Game:
     def __init__(self):
         pygame.init()
         pygame.font.init()
+        pygame.mixer.init()
 
         '''GameInit'''
         frame_size = (gui.card_size[0] * 3 + gui.width_interface_left + gui.width_interface_right,
                       gui.card_size[1] * 2 + gui.height_interface_bottom + gui.height_interface_top)
-        self.screen = pygame.display.set_mode(frame_size)# ,pygame.FULLSCREEN
+        self.screen = pygame.display.set_mode(frame_size)  # ,pygame.FULLSCREEN
         self.clock = pygame.time.Clock()
-
+        self.sound_no_money = pygame.mixer.Sound("../sounds/error0.wav")
         self.interface = gui.GUI(self)
-        reel_rect = pygame.Rect(gui.width_interface_left, gui.height_interface_top, gui.card_size[0] * 3, gui.card_size[1] * 2)
+        reel_rect = pygame.Rect(gui.width_interface_left, gui.height_interface_top, gui.card_size[0] * 3,
+                                gui.card_size[1] * 2)
         self.reel_screen = self.screen.subsurface(reel_rect)
         self.reel = [model.Reel(self.reel_screen, 0),
                      model.Reel(self.reel_screen, gui.card_size[0]),
@@ -32,6 +33,7 @@ class Game:
         self.result = [-1, -1, -1]
 
         self.coins = 0
+        self.coins_won = 0
 
     def game_loop(self):
         while 1:
@@ -39,6 +41,9 @@ class Game:
             self.draw()
             if all((i == self.result[0] and i is not -1) for i in self.result):
                 self.interface.show_winner_window()
+                self.coins_won += 10
+                self.interface.showed_coins_won = self.coins_won
+                self.result[:] = map(lambda _: -1, self.result)
 
     def draw(self):
         self.screen.fill((0, 0, 0))  # fill black
@@ -66,24 +71,34 @@ class Game:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:  # Start Roll
-                    self.interface.hide_winner_window()
-                    if all(i == 0 for i in self.roll):
-                        self.result[:] = map(lambda _: -1, self.result)
-                        self.roll_speed[:] = map(
-                            (lambda _: random.randint(roll_speed_range[0], roll_speed_range[1])),
-                            self.roll_speed)
-                        self.roll[:] = map((lambda _: random.randint(roll_range[0], roll_range[1])), self.roll)
+                    self.start_roll()
                 elif event.key == pygame.K_F3:  # Show FPS
                     self.interface.show_fps_clicked()
                 elif event.key == pygame.K_F4:
                     self.interface.show_winner_window()
                 elif event.key == pygame.K_F5:
-                    pygame.display.toggle_fullscreen()
+                    self.coins_inserted(10)
                 elif event.key == pygame.K_ESCAPE:
                     sys.exit()
 
+    def start_roll(self):
+        self.interface.hide_winner_window()
+        if all(i == 0 for i in self.roll):
+            if self.coins == 0:
+                print("Not enough money")
+                self.sound_no_money.play(maxtime=400)
+                return
+            self.coins = self.coins - 1
+            self.interface.showed_coins = self.coins
+            self.result[:] = map(lambda _: -1, self.result)
+            self.roll_speed[:] = map(
+                (lambda _: random.randint(roll_speed_range[0], roll_speed_range[1])),
+                self.roll_speed)
+            self.roll[:] = map((lambda _: random.randint(roll_range[0], roll_range[1])), self.roll)
+
     def coins_inserted(self, coins):
         self.coins += coins
+        self.interface.showed_coins = self.coins
 
 
 g = Game()
--- PythonSrc/gui.py
+++ PythonSrc/gui.py
@@ -13,17 +13,23 @@ blue = (29, 72, 153)
 
 
 class GUI:
-    def __init__(self,game):
+    def __init__(self, game):
         self.font_arial = pygame.font.SysFont("Arial", 30)
         self.game = game
-        self.interface_bottom = pygame.Rect(0, self.game.screen.get_height() - height_interface_bottom,
-                                            self.game.screen.get_width(), height_interface_bottom)
-        self.interface_top = pygame.Rect(0, 0, self.game.screen.get_height(), height_interface_top)
+        self.interface_bottom = self.game.screen.subsurface(
+            pygame.Rect(0, self.game.screen.get_height() - height_interface_bottom,
+                        self.game.screen.get_width(), height_interface_bottom))
+        self.interface_top = self.game.screen.subsurface(
+            pygame.Rect(0, 0, self.game.screen.get_width(), height_interface_top))
         self.winner_box = pygame.Rect(0, 0, 100, 50)
-        self.winner_box.center = (self.game.screen.get_width() // 2, self.game.screen.get_height() // 2)  # center box on screen
+        # center box on screen
+        self.winner_box.center = (self.game.screen.get_width() // 2, self.game.screen.get_height() // 2)
+
         '''GameStatesInit'''
         self.show_fps = 0
         self.show_winner = 0
+        self.showed_coins = 0
+        self.showed_coins_won = 0
 
     def show_fps_clicked(self):
         self.show_fps = (self.show_fps + 1) % 2
@@ -35,8 +41,8 @@ class GUI:
         self.show_winner = 0
 
     def draw(self):
-        pygame.draw.rect(self.game.screen, yellow, self.interface_bottom)
-        pygame.draw.rect(self.game.screen, yellow, self.interface_top)
+        self.interface_bottom.fill(yellow)
+        self.interface_top.fill(yellow)
         if self.show_fps:
             text_fps = self.font_arial.render("Fps: " + str('{0:.0f}'.format(self.game.clock.get_fps())),
                                               False, white)
@@ -47,3 +53,9 @@ class GUI:
             text_winner_rect = text_winner.get_rect(
                 center=(self.game.screen.get_width() // 2, self.game.screen.get_height() // 2))
             self.game.screen.blit(text_winner, text_winner_rect)
+        text_coins = self.font_arial.render("Coins " + str(self.showed_coins), True, blue)
+        text_coins_won = self.font_arial.render("Coins won: " + str(self.showed_coins_won), True, blue)
+        text_coins_rect = text_coins.get_rect(x=0, y=0)
+        text_coins_won_rect = text_coins_won.get_rect(x=0, y=self.interface_bottom.get_height() // 2)
+        self.interface_bottom.blit(text_coins, text_coins_rect)
+        self.interface_bottom.blit(text_coins_won, text_coins_won_rect)
