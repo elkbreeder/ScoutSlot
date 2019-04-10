b27fb8e9c6ccd40f9be3fa548f3c58eaae2c0242
Gregor Ziegltrum <gregor@trum.eu>
2019-04-10 09:50:06 +0200

    Removed unnecessary files Added comments added gitignore

--- PythonSrc/game.py
+++ PythonSrc/game.py
@@ -5,6 +5,7 @@ import random
 from PythonSrc import gui
 from PythonSrc import model
 
+NO_RESULT = -1
 roll_speed_range = (50, 100)
 roll_range = (50, 100)
 fps = 25
@@ -18,22 +19,22 @@ class Game:
 
         '''GameInit'''
         frame_size = (gui.card_size[0] * 3 + gui.width_interface_left + gui.width_interface_right,
-                      gui.card_size[1] * 2 + gui.height_interface_bottom + gui.height_interface_top)
-        self.screen = pygame.display.set_mode(frame_size)  # ,pygame.FULLSCREEN
+                      gui.card_size[1] * 2 + gui.height_interface_bottom + gui.height_interface_top)  # init frame
+        self.screen = pygame.display.set_mode(frame_size)  # display frame ,pygame.FULLSCREEN
         self.clock = pygame.time.Clock()
         self.sound_no_money = pygame.mixer.Sound("../sounds/error0.wav")
         self.sound_chatter = pygame.mixer.Sound("../sounds/chattering_teeth.wav")
-        self.sound_win = pygame.mixer.Sound("../sounds/win.wav")
-        self.interface = gui.GUI(self)
+        self.sound_win = pygame.mixer.Sound("../sounds/win.wav")  # init all sound effects
+        self.interface = gui.GUI(self)  # init GUI
         reel_rect = pygame.Rect(gui.width_interface_left, gui.height_interface_top, gui.card_size[0] * 3,
                                 gui.card_size[1] * 2)
-        self.reel_screen = self.screen.subsurface(reel_rect)
+        self.reel_screen = self.screen.subsurface(reel_rect)  # define reel subsurface
         self.reel = [model.Reel(self.reel_screen, 0),
                      model.Reel(self.reel_screen, gui.card_size[0]),
-                     model.Reel(self.reel_screen, gui.card_size[0] * 2)]
+                     model.Reel(self.reel_screen, gui.card_size[0] * 2)]  # init all reels
         self.roll = [0, 0, 0]
         self.roll_speed = [0, 0, 0]
-        self.result = [-1, -1, -1]
+        self.result = [NO_RESULT, NO_RESULT, NO_RESULT]
         self.extra_rolls = 50
 
         self.current_extra_rolls = 0
@@ -41,29 +42,32 @@ class Game:
         self.coins_won = 0
 
     def game_loop(self):
-        while 1:
-            self.event_manager()
-            if all(i == 0 for i in self.roll):
+        while 1:  # endless loop
+            self.event_manager()  # manage events
+            if all(i == 0 for i in self.roll):  # if all rolls are stopped
                 self.sound_chatter.stop()
-            for i in range(0, len(self.reel)):
-                if self.roll[i] > 0:
-                    self.roll[i] -= 1
-                    self.reel[i].move(self.roll_speed[i])
-                    if self.current_extra_rolls < self.extra_rolls and self.roll[i] == 0 and (
-                            self.reel[i].get_current_rect().topleft[1] < self.reel_screen.get_height() // 7 or
-                            self.reel[i].get_current_rect().topleft[1] > self.reel_screen.get_height() * 2 // 7):
-                        # if the result doesn't look clear, turn the wheel a bit more
-                        self.roll[i] = 1
-                        self.current_extra_rolls += 1
-                    elif self.roll[i] == 0:
-                        self.result[i] = self.reel[i].get_current_id()
+            for i in range(0, len(self.reel)):  # loop over all reels
+                if self.roll[i] == 0:  # skip if roll doesn't run
+                    continue
+                self.roll[i] -= 1
+                self.reel[i].move(self.roll_speed[i])  # move roll
+                if self.current_extra_rolls < self.extra_rolls and self.roll[i] == 0 and (
+                        self.reel[i].get_current_rect().topleft[1] < self.reel_screen.get_height() // 7 or
+                        self.reel[i].get_current_rect().topleft[1] > self.reel_screen.get_height() * 2 // 7):
+                    # if the result doesn't look clear, turn the wheel a bit more but max self.extra_rolls times
+                    # per spin
+                    self.roll[i] = 1
+                    self.current_extra_rolls += 1
+                elif self.roll[i] == 0:
+                    self.result[i] = self.reel[i].get_current_id()  # save the result if the current roll stops
             self.draw()
-            if all((j == self.result[0] and j is not -1) for j in self.result):
-                self.sound_win.play()
+            if all((j == self.result[0] and j is not NO_RESULT) for j in
+                   self.result):  # if all reels are stopped and show the same
+                self.sound_win.play()  # player won
                 self.interface.show_winner_window()
                 self.coins_won += 10
                 self.interface.showed_coins_won = self.coins_won
-                self.result[:] = map(lambda _: -1, self.result)
+                self.result[:] = map(lambda _: NO_RESULT, self.result)
             self.clock.tick(fps)
 
     def draw(self):
@@ -84,7 +88,7 @@ class Game:
                     self.start_roll()
                 elif event.key == pygame.K_F3:  # Show FPS
                     self.interface.show_fps_clicked()
-                elif event.key == pygame.K_F4:
+                elif event.key == pygame.K_F4:  # Show Winner Window
                     self.interface.show_winner_window()
                 elif event.key == pygame.K_F5:
                     self.coins_inserted(10)
@@ -93,15 +97,15 @@ class Game:
 
     def start_roll(self):
         self.interface.hide_winner_window()
-        if all(i == 0 for i in self.roll):
-            self.sound_chatter.play(-1)
+        if all(i == 0 for i in self.roll):  # if no reel runs
+            self.sound_chatter.play(-1)  # start rolling
             self.current_extra_rolls = 0
             if self.coins == 0:
                 self.sound_no_money.play(maxtime=400)
                 return
             self.coins = self.coins - 1
             self.interface.showed_coins = self.coins
-            self.result[:] = map(lambda _: -1, self.result)
+            self.result[:] = map(lambda _: NO_RESULT, self.result)
             self.roll_speed[:] = map(
                 (lambda _: random.randint(roll_speed_range[0], roll_speed_range[1])),
                 self.roll_speed)
@@ -113,4 +117,4 @@ class Game:
 
 
 g = Game()
-g.game_loop()
+g.game_loop()  # start game loop
