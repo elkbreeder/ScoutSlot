3f88ee52f3597e92b9f0e8f0895265070a93bef2
Gregor <gregor@trum.eu>
2019-05-11 17:50:50 +0200

    added camera code

--- PythonSrc/cam.py
+++ PythonSrc/cam.py
@@ -1,10 +1,12 @@
 import pygame
 import pygame.camera
+import datetime
 import os
 
 
 class Camera:
     def __init__(self):
+        pygame.camera.init()
         if os.name == 'nt':  #
             print("Windows doesn't support camera functionality")
             return
@@ -14,20 +16,18 @@ class Camera:
         if os.name == 'nt':  #
             print("Windows doesn't support camera functionality")
             return
-        pygame.camera.init()
 
         self.cam.start()
         img = self.cam.get_image()
-        pygame.image.save(img, "filename.jpg")
-
+        now = datetime.datetime.now()
+        pygame.image.save(img, now.strftime("%m.%d %H.%M.%S") + ".jpg")
         self.cam.stop()
 
     def capture_next_winner(self):
-        print("NOT IMPLEMENTED")
+        # print("NOT IMPLEMENTED")
         self.capture("~/images/")
 
 
 if __name__ == '__main__':
     camera = Camera()
     camera.capture('test.jpg')
-
